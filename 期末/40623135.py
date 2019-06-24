f59515be68d5617dd3598ded80af2ca70e7a9fbc
40623135 <40623135@nfu.edu.tw>
2019-06-24 16:59:56 +0800

    程式

--- 期末/40623135.py
+++ 期末/40623135.py
@@ -2,9 +2,6 @@ import vrep
 import keyboard
 from time import sleep
 import sys, math
-# child threaded script: 
-# 內建使用 port 19997 若要加入其他 port, 在  serve 端程式納入
-#simExtRemoteApiStart(19999)
  
 vrep.simxFinish(-1)
  
@@ -40,25 +37,23 @@ def play():
     KickBallV =7200
     Move_Minus =-0.1         
     Move_Plus =0.1
-    L_KickBallVel = (math.pi/180)*KickBallV     
+    L_KickBallVel = (math.pi/180)*KickBallV
     R_KickBallVel = -(math.pi/180)*KickBallV
-    pushball_vel = 100        
-
-
-
+    pushball_vel = 100
     while True:
-        try:
-            if keyboard.is_pressed('a'):
-                vrep.simxSetJointTargetVelocity(clientID,LRev_handle,L_KickBallVel,vrep.simx_opmode_oneshot_wait)
-            elif keyboard.is_pressed('l'):
-                vrep.simxSetJointTargetVelocity(clientID,RRev_handle,R_KickBallVel,vrep.simx_opmode_oneshot_wait)
-            elif keyboard.is_pressed('UP'):
-                vrep.simxSetJointTargetVelocity(clientID,Push_handle,Pushball_Vel,vrep.simx_opmode_oneshot_wait)
-            else:
-                vrep.simxSetJointTargetVelocity(clientID,RRev_handle,L_KickBallVel,vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(clientID,LRev_handle,R_KickBallVel,vrep.simx_opmode_oneshot_wait)
-                vrep.simxSetJointTargetVelocity(clientID,Push_handle,Pushball_Vel,vrep.simx_opmode_oneshot_wait)
-        except:
-            break
+            try:
+                if keyboard.is_pressed('a'):
+                    vrep.simxSetJointTargetVelocity(clientID,LRev_handle,L_KickBallVel,vrep.simx_opmode_oneshot_wait)
+                elif keyboard.is_pressed('l'):
+                    vrep.simxSetJointTargetVelocity(clientID,RRev_handle,R_KickBallVel,vrep.simx_opmode_oneshot_wait)
+                elif keyboard.is_pressed('UP'):
+                    vrep.simxSetJointTargetVelocity(clientID,Push_handle,-pushball_vel,vrep.simx_opmode_oneshot_wait)
+                else:
+                    vrep.simxSetJointTargetVelocity(clientID,RRev_handle,L_KickBallVel,vrep.simx_opmode_oneshot_wait)
+                    vrep.simxSetJointTargetVelocity(clientID,LRev_handle,R_KickBallVel,vrep.simx_opmode_oneshot_wait)
+                    vrep.simxSetJointTargetVelocity(clientID,Push_handle,pushball_vel,vrep.simx_opmode_oneshot_wait)
+            except:
+                break
+                
 start()
 play()
